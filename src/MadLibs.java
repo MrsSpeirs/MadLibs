f9aa38e813e2951fc3ea6db7b528d890efc22c1e
MrsSpeirs <sspeirs@dasd.org>
2020-09-15 05:41:46 -0400

    Initial commit. Class header and main method with directions

---
+++ src/MadLibs.java
@@ -0,0 +1,14 @@
+/*AUTHOR (Put your name here)
+ * 9/15/2020, 5:23 AM
+ * Use the Scanner class to get input from the user.
+ * Write a very short story with at least four missing words -
+ * 3 sentences will be sufficient.  Request the user to input
+ * the missing words of various types (adjective, noun, etc.).
+ * Output your story to the user with their input in the "blanks"
+ * MadLibs style.   */
+
+public class MadLibs {
+    public static void main(String args[]) {
+
+    }
+}
